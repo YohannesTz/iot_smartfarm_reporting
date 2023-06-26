b396b1c00439e8d0400a7ce748692eb1b25e5df0
Yohannes Tezera <yohannes222ethiopia@gmail.com>
2023-06-26 15:47:35 +0300

    changed url

--- src/util/Constants.js
+++ src/util/Constants.js
@@ -1,3 +1,4 @@
-const BASE_URL = 'https://sleeping-dragon.000webhostapp.com/proj/';
+//const BASE_URL = 'https://sleeping-dragon.000webhostapp.com/proj/';
+const BASE_URL = 'https://farmig.000webhostapp.com/iot/';
 
 export { BASE_URL };
